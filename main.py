acfe53a2d06670fce596872afda1ecbe813d84d2
Asdfire <asdfirepl@gmail.com>
2021-10-28 09:56:30 +0200

    added pca lda, not tested

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-
 import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
     return dict
+
+
 trainfile = "data/data_batch_1"
 traindict=unpickle(trainfile)
-testfile = "data/test_batch"
-testdict=unpickle(testfile)
 trainlabels=traindict[b'labels']
 traindata=traindict[b'data']
+traindata = traindata/255
+
+testfile = "data/test_batch"
+testdict=unpickle(testfile)
+testlabels=testdict[b'labels']
+testdata=testdict[b'data']
+testdata = testdata/255
+
 
 #splitter
 trn = [None] * 10
@@ -21,5 +32,15 @@ for b in range (10):
             trn[b]=np.concatenate((trn[b], np.array([traindata[x]])), axis=0)
     trn[b]=np.delete(trn[b], 0, 0)
 
-print(trn[0])
-    
+#print(trn[0])
+n_components = 9
+
+#PCA
+pca = PCA(n_components = n_components)
+trn_pca_set = pca.fit_transform(traindata)
+tst_pca_set = pca.transform(testdata)
+
+#LDA
+lda = LDA(n_components = n_components)
+trn_lda_set = lda.fit_transform(traindata, trainlabels)
+tst_lda_set = lda.transform(testdata)
