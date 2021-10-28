5965db2b94b2f67b43db1187ec4f2b7804824c9e
Asdfire <asdfirepl@gmail.com>
2021-10-28 11:30:44 +0200

    removed LDA, first working code

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ def unpickle(file):
 
 trainfile = "data/data_batch_1"
 traindict=unpickle(trainfile)
-trainlabels=traindict[b'labels']
+trainlabelsl=traindict[b'labels']
+trainlabels=np.array(trainlabelsl)
 traindata=traindict[b'data']
 traindata = traindata/255
 
 testfile = "data/test_batch"
 testdict=unpickle(testfile)
-testlabels=testdict[b'labels']
+testlabelsl=testdict[b'labels']
+testlabels=np.array(testlabelsl)
 testdata=testdict[b'data']
 testdata = testdata/255
 
@@ -44,7 +46,7 @@ def est_params(trn_set, trn_targets):
     '''
     #Get the data dimension
     _, d = trn_set.shape 
-    
+    print(f'Trn shape: {trn_set.shape}')
     #Zero arrays for storing means and covs.
     means = np.zeros((10,d))
     covs = np.zeros((10, d, d))
@@ -52,7 +54,9 @@ def est_params(trn_set, trn_targets):
     #For each class compute mean and cov.
     for i in range(10):
         indx = trn_targets == i
+        print(f'index: {indx}')
         means[i] = np.mean(trn_set[indx], axis = 0)
+        print(f'Means of: {i} :{means[i]}')
         covs[i] = np.cov(trn_set[indx].T)
     return means, covs
 
@@ -93,7 +97,7 @@ for b in range (10):
 
 #print(trn[0])
 '''
-n_components = 9
+n_components = 100
 
 #Start of stolen code again 
 print(traindata)
@@ -101,30 +105,30 @@ print(traindata)
 pca = PCA(n_components = n_components)
 trn_pca_set = pca.fit_transform(traindata)
 tst_pca_set = pca.transform(testdata)
-
+'''
 #LDA
 lda = LDA(n_components = n_components)
 trn_lda_set = lda.fit_transform(traindata, trainlabels)
 tst_lda_set = lda.transform(testdata)
-
+'''
 #Proportion of Variance
 pov_pca = np.sum(pca.explained_variance_ratio_)
-pov_lda = np.sum(lda.explained_variance_ratio_)
+#pov_lda = np.sum(lda.explained_variance_ratio_)
 
 #%%Classification
 #Compute the parameters for each PCA and LDA reduced data.
 pca_means, pca_covs = est_params(trn_pca_set, trainlabels)
-lda_means, lda_covs = est_params(trn_lda_set, trainlabels)
+#lda_means, lda_covs = est_params(trn_lda_set, trainlabels)
 
 #Compute predictions
 pca_pred = predict(tst_pca_set, pca_means, pca_covs)
-lda_pred = predict(tst_lda_set, lda_means, lda_covs)
+#lda_pred = predict(tst_lda_set, lda_means, lda_covs)
 
 #Compute accuracy
 pca_acc = np.sum(pca_pred == testlabels)/len(testlabels) * 100
-lda_acc = np.sum(lda_pred == testlabels)/len(testlabels) * 100
+#lda_acc = np.sum(lda_pred == testlabels)/len(testlabels) * 100
 print("PCA Accuracy: {:.2f}".format(pca_acc))
-print("LDA Accuracy: {:.2f}".format(lda_acc))
+#print("LDA Accuracy: {:.2f}".format(lda_acc))
 
 
                      
