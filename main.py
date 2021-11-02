90982803194d66e961e9b55406d24f9e6c46dae4
asdfire1 <45483619+asdfire1@users.noreply.github.com>
2021-11-02 11:12:17 +0100

    code cleaning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import numpy as np
 from sklearn.decomposition import PCA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from scipy.stats import multivariate_normal as norm
 
 def unpickle(file):
@@ -24,7 +22,7 @@ trainlabelsl2=traindict[b'labels']
 trainlabels2=np.array(trainlabelsl2)
 traindata2=traindict2[b'data']
 traindata2 = traindata2/255
-'''
+
 trainfile3 = "data/data_batch_3"
 traindict3=unpickle(trainfile3)
 trainlabelsl3=traindict[b'labels']
@@ -41,14 +39,15 @@ traindata4 = traindata4/255
 
 trainfile5 = "data/data_batch_5"
 traindict5=unpickle(trainfile5)
-trainlabelsl3=traindict[b'labels']
-trainlabels3=np.array(trainlabelsl3)
-traindata3=traindict3[b'data']
-traindata3 = traindata3/255
-'''
+trainlabelsl5=traindict[b'labels']
+trainlabels5=np.array(trainlabelsl5)
+traindata5=traindict5[b'data']
+traindata5 = traindata3/255
 
-traindata=np.concatenate((traindata1, traindata2))
-trainlabels=np.concatenate((trainlabels1,trainlabels2))
+#traindata=traindata1
+#trainlabels=trainlabels1
+traindata=np.concatenate((traindata1, traindata2, traindata3, traindata4, traindata5))
+trainlabels=np.concatenate((trainlabels1,trainlabels2, trainlabels3, trainlabels4, trainlabels5))
 
 
 testfile = "data/test_batch"
@@ -58,7 +57,7 @@ testlabels=np.array(testlabelsl)
 testdata=testdict[b'data']
 testdata = testdata/255
 
-#STOLEN FROM EXERCISES
+#Functions from the exercises
 def est_params(trn_set, trn_targets):
     '''
     Function for estimating the parameters for multiple gaussian distributions.
@@ -79,7 +78,6 @@ def est_params(trn_set, trn_targets):
     '''
     #Get the data dimension
     _, d = trn_set.shape 
-    #print(f'Trn shape: {trn_set.shape}')
     #Zero arrays for storing means and covs.
     means = np.zeros((10,d))
     covs = np.zeros((10, d, d))
@@ -87,9 +85,7 @@ def est_params(trn_set, trn_targets):
     #For each class compute mean and cov.
     for i in range(10):
         indx = trn_targets == i
-        #print(f'index: {indx}')
         means[i] = np.mean(trn_set[indx], axis = 0)
-        #print(f'Means of: {i} :{means[i]}')
         covs[i] = np.cov(trn_set[indx].T)
     return means, covs
 
@@ -117,51 +113,29 @@ def predict(tst_set, means, covs):
     probs = np.c_[tuple(probs)]
     preds = np.argmax(probs, axis = 1)
     return preds
-#END OF STOLEN CODE
-'''
-#splitter
-trn = [None] * 10
-for b in range (10):
-    trn[b]=np.array([np.zeros(3072)])
-    for x in range(len(traindict[b'labels'])):
-        if trainlabels[x]==b:
-            trn[b]=np.concatenate((trn[b], np.array([traindata[x]])), axis=0)
-    trn[b]=np.delete(trn[b], 0, 0)
+#End of functions from the exercises
 
-#print(trn[0])
-'''
-n_components = 100
+n_components = 190
 
-#Start of stolen code again 
+#Using the functions
 #print(traindata)
 #PCA
 pca = PCA(n_components = n_components)
 trn_pca_set = pca.fit_transform(traindata)
 tst_pca_set = pca.transform(testdata)
-'''
-#LDA
-lda = LDA(n_components = n_components)
-trn_lda_set = lda.fit_transform(traindata, trainlabels)
-tst_lda_set = lda.transform(testdata)
-'''
-#Proportion of Variance
-pov_pca = np.sum(pca.explained_variance_ratio_)
-#pov_lda = np.sum(lda.explained_variance_ratio_)
 
-#%%Classification
-#Compute the parameters for each PCA and LDA reduced data.
+
+#Compute the distribution parameters for PCA
 pca_means, pca_covs = est_params(trn_pca_set, trainlabels)
-#lda_means, lda_covs = est_params(trn_lda_set, trainlabels)
 
-#Compute predictions
+
+#Classify the data
 pca_pred = predict(tst_pca_set, pca_means, pca_covs)
-#lda_pred = predict(tst_lda_set, lda_means, lda_covs)
 
-#Compute accuracy
+
+#Calculating accuraccy
 pca_acc = np.sum(pca_pred == testlabels)/len(testlabels) * 100
-#lda_acc = np.sum(lda_pred == testlabels)/len(testlabels) * 100
 print("PCA Accuracy: {:.2f}".format(pca_acc))
-#print("LDA Accuracy: {:.2f}".format(lda_acc))
 
 
                      
