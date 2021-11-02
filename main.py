e37e7f88b3091a8361d3080fbed7b0396dad5086
asdfire1 <45483619+asdfire1@users.noreply.github.com>
2021-11-02 11:53:31 +0100

    changed timers a bit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-
+stime=time.time()
 trainfile = "data/data_batch_1"
 traindict=unpickle(trainfile)
 trainlabelsl=traindict[b'labels']
@@ -23,12 +23,16 @@ testlabelsl=testdict[b'labels']
 testlabels=np.array(testlabelsl)
 testdata=testdict[b'data']
 testdata = testdata/255
-
+ltime = time.time() - stime
+print("Data loading time: {:2.0f}m{:2.0f}s".format(ltime//60, ltime%60))
 #Code from exercises
+start_time = time.time()
 n_components=190
 pca = PCA(n_components = n_components)
 trn_pca_set = pca.fit_transform(traindata)
 tst_pca_set = pca.transform(testdata)
+pca_time = time.time() - start_time     #Compute the training/fiting time in seconds
+print("PCA time: {:2.0f}m{:2.0f}s".format(pca_time//60, pca_time%60))
 #A list of the class names.
 classes = np.arange(10)
 
@@ -39,9 +43,9 @@ n_iter = 3000
 #is the RBF (Gaussian Kernel).
 svc = svm.SVC(max_iter = n_iter)
 
-start_time = time.time()    #Time in seconds when starting the training/fiting.
+start_time_s = time.time()    #Time in seconds when starting the training/fiting.
 svc.fit(trn_pca_set, trainlabels)   #Train/fit the SVC
-fit_time = time.time() - start_time     #Compute the training/fiting time in seconds
+fit_time = time.time() - start_time_s     #Compute the training/fiting time in seconds
 print("Training Time: {:2.0f}m{:2.0f}s".format(fit_time//60, fit_time%60))
 
 #Predictions
